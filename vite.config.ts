1468631209bf3dfa5089e17a42adb74ef03d883f
claude.seguret <claude.seguret@laposte.fr>
2026-03-04 07:16:00 +0100

    Ajout app android

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,13 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        rollupOptions: {
+          input: {
+            main: path.resolve(__dirname, 'index.html'),
+          }
+        }
       }
     };
 });
